64887ccb133b64a6a6d04b394f9b027c24274303
Abai369 <jcaleb1110@gmail.com>
2025-03-27 18:26:57 -0700

    feat cart fix register.html

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),  # esta línea lo conecta todo
+    path('cart/', include('cart.urls', namespace='cart')),
+   path('accounts/', include('accounts.urls')),
 ]
+
+# Esto sirve las imágenes subidas en modo desarrollo
+if settings.DEBUG:
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
