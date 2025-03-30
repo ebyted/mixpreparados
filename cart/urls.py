2f02790290b9ef39cb6a901edabbcdb15fbc1065
Abai369 <jcaleb1110@gmail.com>
2025-03-29 18:54:01 -0700

    feat carrito funcional

--- cart/urls.py
+++ cart/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 
-app_name = 'cart'  # 👈 Esto es clave para el namespace
+app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_view, name='cart_view'),
     path('increase/<int:item_id>/', views.increase_quantity, name='increase_quantity'),
     path('decrease/<int:item_id>/', views.decrease_quantity, name='decrease_quantity'),
     path('remove/<int:item_id>/', views.remove_item, name='remove_item'),
+    path('checkout/', views.checkout_view, name='checkout'),  # ✅ nombre correcto
 ]
