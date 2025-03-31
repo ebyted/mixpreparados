2a22bea92b35b5d27a8373d3ccb2fa081766363f
Edgar <ebyted@gmail.com>
2025-03-31 01:52:19 -0700

    feat: update Django settings for static files configuration

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for config project.
 
@@ -132,3 +133,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
