562c9deebef7698f77c01df7db2d5784cc8e8140
Abai369 <jcaleb1110@gmail.com>
2025-05-12 15:12:25 -0700

    add stripe to requirements

--- config/settings.py
+++ config/settings.py
@@ -12,8 +12,8 @@ env = environ.Env(
 environ.Env.read_env(BASE_DIR / ".env")
 
 # --- STRIPE
-STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY")
-STRIPE_PUBLISHABLE_KEY = env("STRIPE_PUBLISHABLE_KEY")
+STRIPE_SECRET_KEY = env("STRIPE_SECRET_KEY", default="")
+STRIPE_PUBLISHABLE_KEY = env("STRIPE_PUBLISHABLE_KEY", default="")
 
 
 # --- SECURITY
