cb6b3575fb66da0e2c305f41179135e2762a275a
Abai369 <jcaleb1110@gmail.com>
2025-05-15 10:55:34 -0700

    fix: remove duplicate products URL path in urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -5,12 +5,14 @@ from django.conf.urls.static import static
 from debug.views import debug_info
 
 urlpatterns = [
+    path('products/', include('products.urls', namespace='products')),             
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),  # esta línea lo conecta todo
     path('cart/', include('cart.urls', namespace='cart')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('products/', include('products.urls', namespace='products')),
+
+    path('', include('pages.urls')),
 #path('testimonials/', include('testimonials.urls')),
     path('debug/', debug_info),
 ]
